d1d54680922a827d03d54bcbf3d38943af378f63
Devon Finninger <devonfinninger@gmail.com>
2014-06-04 17:54:32 -0500

    adds config file for window size

--- lib/player.rb
+++ lib/player.rb
@@ -10,6 +10,9 @@ def media_path(file); File.expand_path "../media/#{file}", File.dirname(__FILE__
 class Player
    FOOTROOM = 54
    HEADROOM = 20
+   SIDEROOM = 0
+   WIDTH    = SIDEROOM * 2
+   HEIGHT   = FOOTROOM + HEADROOM
    def initialize(window)
       @window = window
       @image = Gosu::Image.new(window, media_path("characters/Character Boy.png"), false)
@@ -36,9 +39,13 @@ class Player
    def move
       @x += @vel_x
       @y += @vel_y
-      @x %= @window.width
-      if @y >= (@window.height-FOOTROOM)
-         @y =  (@window.height-FOOTROOM)
+      if @x >= @window.width-SIDEROOM
+         @x =  @window.width-SIDEROOM
+      elsif @x <= SIDEROOM
+         @x = SIDEROOM
+      end
+      if @y >= @window.height-FOOTROOM
+         @y =  @window.height-FOOTROOM
          @vel_y = 0
       elsif @y <= HEADROOM
          @y =  HEADROOM
